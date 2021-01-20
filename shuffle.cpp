7e999dc1e96de562a9eee2be13f4b69500d66289
David Ortega <35351362+dortegaa@users.noreply.github.com>
2021-01-20 12:07:42 -0600

    Update shuffle.cpp

--- shuffle.cpp
+++ shuffle.cpp
@@ -3,19 +3,16 @@
 using namespace std;
 int main()
 {
-    float aleatorio;
-     int a[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
- 
+    int a[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    unsigned seed = time(NULL); // use of time(NULL) for initialize seed with different value every time it is run
+
     int n = sizeof(a) / sizeof(a[0]);
- 
-    //shuffle_array(a, n);
-    unsigned seed = time(NULL);
-    for (int j=0; j < 10; j++) 
+    for (int j=0; j < 10; j++) // test 10 times the random order
     {
-        shuffle(a, a + n, default_random_engine(seed));
-        for (int i = 0; i < n; ++i)
-        cout << a[i] << " ";
-    cout << endl;
+        shuffle(a, a + n, default_random_engine(seed)); // function suffle
+        for (int i = 0; i < n; ++i) 
+            cout << a[i] << " "; // output element by element
+        cout << endl;
     }
 
     return 0;
